e259f42d93a416f04dec70a7adb5428a11528055
Sinder <rasul_sadullayev@mail.ru>
2024-09-18 22:33:06 +0300

    main created

---
+++ src/js/script.js
@@ -0,0 +1,17 @@
+function openModal(title, description) {
+    document.getElementById("modal-title").innerText = title;
+    document.getElementById("modal-description").innerText = description;
+    document.getElementById("modal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("modal").style.display = "none";
+}
+
+// Закрытие модального окна при клике вне его
+window.onclick = function (event) {
+    const modal = document.getElementById("modal");
+    if (event.target == modal) {
+        closeModal();
+    }
+}
